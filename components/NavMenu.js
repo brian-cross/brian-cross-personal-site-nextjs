cb1d08c4c58ed3a0bbe460957dbe4ba35eabcf65
Brian Cross <brian.cross.1977@gmail.com>
2021-08-19 22:30:24 -0600

    Wrap nav menu in portal component

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -1,37 +1,40 @@
 import Link from "next/link";
 import { sizes } from "../styles/theme";
+import Portal from "./Portal";
 
 export default function NavMenu({ onClick }) {
   return (
     <>
-      <div className="nav-menu">
-        <div className="container">
-          <nav onClick={onClick}>
-            <ul>
-              <li>
-                <Link href="/">
-                  <a className="h1">Home</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/about">
-                  <a className="h1">About</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contact">
-                  <a className="h1">Contact</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <a className="h1">Services</a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+      <Portal selector="#modal-root">
+        <div className="nav-menu">
+          <div className="container">
+            <nav onClick={onClick}>
+              <ul>
+                <li>
+                  <Link href="/">
+                    <a className="h1">Home</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/about">
+                    <a className="h1">About</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/contact">
+                    <a className="h1">Contact</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services">
+                    <a className="h1">Services</a>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-      </div>
+      </Portal>
       <style jsx>{`
         .nav-menu {
           position: fixed;
@@ -39,7 +42,6 @@ export default function NavMenu({ onClick }) {
           left: 0;
           width: 100vw;
           height: ${sizes.fullHeight};
-          z-index: 1;
           display: flex;
           align-items: center;
           background: black;
