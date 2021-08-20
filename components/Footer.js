412230a098fef210a31ab2381479c692f6f3385b
Brian Cross <brian.cross.1977@gmail.com>
2021-08-19 22:28:19 -0600

    Moved global CSS into footer component

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,11 @@ export default function Footer() {
       &copy; {new Date().getFullYear()} - Brian Cross
       <style jsx>{`
         footer {
+          display: flex;
           justify-content: center;
+          padding: 1em 0;
+          font-weight: 300;
+          text-transform: uppercase;
         }
       `}</style>
     </footer>
