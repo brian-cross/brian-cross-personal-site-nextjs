1b175ac0859c53361d5ecbbd39b29b7d087a016d
Brian Cross <brian.cross.1977@gmail.com>
2021-08-29 08:45:01 -0600

    Simplified code in useSmoothScroll hook

--- hooks/useSmoothScroll.js
+++ hooks/useSmoothScroll.js
@@ -3,48 +3,42 @@ import { ScrollTrigger } from "../gsap";
 
 export function useSmoothScroll() {
   useEffect(() => {
-    let locoScroll;
+    const LocomotiveScroll = require("locomotive-scroll").default;
+    const container = document.getElementById("smooth-scroll-container");
 
-    function initSmoothScroll() {
-      const LocomotiveScroll = require("locomotive-scroll").default;
-      const container = document.getElementById("smooth-scroll-container");
+    const locoScroll = new LocomotiveScroll({
+      el: container,
+      smooth: true,
+    });
 
-      locoScroll = new LocomotiveScroll({
-        el: container,
-        smooth: true,
-      });
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-      locoScroll.on("scroll", ScrollTrigger.update);
+    ScrollTrigger.scrollerProxy(container, {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      pinType: container.style.transform ? "transform" : "fixed",
+    });
 
-      ScrollTrigger.scrollerProxy(container, {
-        scrollTop(value) {
-          return arguments.length
-            ? locoScroll.scrollTo(value, 0, 0)
-            : locoScroll.scroll.instance.scroll.y;
-        },
-        getBoundingClientRect() {
-          return {
-            top: 0,
-            left: 0,
-            width: window.innerWidth,
-            height: window.innerHeight,
-          };
-        },
-        pinType: container.style.transform ? "transform" : "fixed",
-      });
+    ScrollTrigger.defaults({
+      scroller: container,
+    });
 
-      ScrollTrigger.defaults({
-        scroller: container,
-      });
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
 
-      ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-      ScrollTrigger.refresh();
-
-      const resizeObserver = new ResizeObserver(() => ScrollTrigger.refresh());
-      resizeObserver.observe(container);
-    }
-
-    initSmoothScroll();
+    const resizeObserver = new ResizeObserver(() => ScrollTrigger.refresh());
+    resizeObserver.observe(container);
 
     return () => locoScroll.destroy();
   }, []);
