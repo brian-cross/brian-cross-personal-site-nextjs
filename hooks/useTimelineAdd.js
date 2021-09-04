4a27652ae69195d342c06f70af5b1422ff14bcc8
Brian Cross <brian.cross.1977@gmail.com>
2021-09-03 20:27:35 -0600

    Add tween to useEffect dependency array

--- hooks/useTimelineAdd.js
+++ hooks/useTimelineAdd.js
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 
 export function useTimelineAdd(tl, tween, position) {
   useEffect(() => {
-    if (tl === undefined) return;
+    if (tl === undefined || tween === undefined) return;
 
     tl.add(tween, position);
-  }, [tl]);
+  }, [tl, tween]);
 }
