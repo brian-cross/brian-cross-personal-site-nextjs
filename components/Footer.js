3829313ea029679568d142cfd4d88df8787429c9
Brian Cross <brian.cross.1977@gmail.com>
2021-09-09 21:52:17 -0600

    Changed footer layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,35 +1,26 @@
-import { colors } from "../styles/theme";
+import { colors, sizes } from "../styles/theme";
 
 export default function Footer() {
   return (
     <>
       <footer>
-        <div className="copyright">
-          &copy; {new Date().getFullYear()} - Brian Cross
-        </div>
-        <div>
-          This website is{" "}
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://github.com/brian-cross/brian-cross-personal-site-nextjs"
-          >
-            open source
-          </a>{" "}
+        <div className="container">
+          <div className="copyright">
+            &copy; {new Date().getFullYear()} - Brian Cross
+          </div>
         </div>
       </footer>
       <style jsx>{`
         footer {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          gap: 0.5em;
           padding: 1em 0;
           font-weight: 300;
           background-color: ${colors.footerBg};
+        }
 
-          a {
-            font-size: inherit;
+        .copyright {
+          display: flex;
+          @media (max-width: ${sizes.mobileBreakpoint}) {
+            justify-content: center;
           }
         }
       `}</style>
