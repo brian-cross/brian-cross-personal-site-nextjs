fc478220da7a64582563766b7229797faf184e3a
Brian Cross <brian.cross.1977@gmail.com>
2021-10-06 20:27:07 -0600

    Added Google analytics tags

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-MEJVTC7MGK"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-MEJVTC7MGK');
+          </script>
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
             rel="stylesheet"
